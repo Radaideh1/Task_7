2fac7fb8a86f883c680b3e76a69479c5fb67daa2
Radaideh <radaideh7484@gmail.com>
2025-05-08 20:16:20 +0300

    I end the Menu System !

---
+++ main.cpp
@@ -0,0 +1,71 @@
+﻿#include "Header.h"
+
+
+int main() {
+
+	int q;
+	cout << "======\t\tWelcom\t\t======\n\nChose from one of this :\n \
+\n1- Computer Factorial.\n2- Compute Power.\n3- Swap Characters.\
+\n4- Calculate Salary.\n5- Area of Triangle.\n6- Area of circle.\
+\n7- Volume of Cylinder.\n8- Series Sum.\n9- 3D Volume <Cube>\
+\n10- 3D Volume <Pyramid>.\n11- Calculator (4 operations).\
+\n12- Recursive Sum to N.\n13- Exit." << endl;
+	cin >> q;
+
+	switch (q) {
+	case 1: {
+		break;
+	}
+	case 2: {
+		break;
+	}
+	case 3: {
+		break;
+	}
+	case 4: {
+		break;
+	}
+	case 5: {
+		break;
+	}
+	case 6: {
+		break;
+	}
+	case 7: {
+		break;
+	}
+	case 8: {
+		break;
+	}
+	case 9: {
+		break;
+	}
+	case 10: {
+		break;
+	}
+	case 11: {
+		break;
+	}
+	case 12: {
+		break;
+	}
+	case 13: {
+		break;
+	}
+
+
+
+
+	}// for switch
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
